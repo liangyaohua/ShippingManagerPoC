6eb93c401f04d12bd03d80349d046847824b9d5d
Yaohua LIANG <alex19891013@gmail.com>
2014-04-23 17:15:32 +0200

    Add function updateStatus
    almost finish

--- WebContent/app.js
+++ WebContent/app.js
@@ -3,6 +3,9 @@
 // it initializes the HTML page for mobile use and provides animated page handling
 var app = new sap.m.App("ShippingManager", {initialPage:"homepage"});
 
+var aShipment = "";
+var oPath = "";
+
 // create a list for displaying shipment details
 var oList = new sap.m.List({
 	items: [
@@ -25,7 +28,8 @@ var oList = new sap.m.List({
 var oButton = new sap.m.Button({
 	text: "Shipment start",
 	icon: "sap-icon://shipping-status",
-	enabled: false
+	enabled: false,
+	press: updateStatus
 });
 
 // create a Search Field
@@ -33,11 +37,13 @@ var oSearchField = new sap.m.SearchField({
 	placeholder: "shippment number",
 	width: "100%",
 	search: function(oEvent){
-		var aShipment = oEvent.getParameter("query");
+		aShipment = oEvent.getParameter("query");
 		if(aShipment != "") {
 			
-			var oPath = "/SHIPMENT('"+ aShipment +"')";
+			oPath = "/SHIPMENT('"+ aShipment +"')";
 	 		
+			// refresh OData model and read the data
+			oModel.refresh();
 			oModel.read(oPath, null, null, true, function(oData){
 				if(JSON.stringify(oData.Tknum) != '""' && oData.Tknum != undefined){
 					// Clear old list, rebind data, add new list
@@ -45,8 +51,10 @@ var oSearchField = new sap.m.SearchField({
 					oList.bindElement(oPath);
     				homepage.addContent(oList);
     				
-    				if(JSON.stringify(oData.Sttrg) == '"7"')
+    				if(JSON.stringify(oData.Sttrg) == '"5"')
     					oButton.setEnabled();
+    				else
+    					oButton.setEnabled(false);
 				} else {
 					alert(aShipment + " doesn't exist");
 				}
@@ -77,8 +85,39 @@ var homepage = new sap.m.Page("homepage", {
     })           
   });
 
-//add homepage to the App
+// add homepage to the App
 app.addPage(homepage); 
 
-//place the App into the HTML document
+// place the App into the HTML document
 app.placeAt("content"); 
+
+// update the status of a shipment
+function updateStatus(){
+	// disable the button after it is clicked already
+	oButton.setEnabled(false);
+	// read update result
+	oModel2.read(oPath, null, null, true, function(oData){
+		if(JSON.stringify(oData.Status) == '"6"'){
+			// refresh OData model and read the data
+			oModel.refresh();
+			oModel.read(oPath, null, null, true, function(oData){
+				// Clear old list, rebind data, add new list
+				homepage.removeContent(oList);
+				oList.bindElement(oPath);
+				homepage.addContent(oList);
+				 
+				if(JSON.stringify(oData.Sttrg) == '"5"'){
+					oButton.setEnabled(true);
+				} else {
+					alert("Update successful");
+				}
+			},function(){
+				return;
+			});
+		} else {
+			alert("Update failed, please it try again");
+		}
+	},function(){
+		alert("Connection failed");
+	});
+}
