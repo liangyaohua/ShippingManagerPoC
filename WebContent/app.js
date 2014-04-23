7794ed4389bd92b1a20cdb77d3957731d4d044ea
Yaohua LIANG <alex19891013@gmail.com>
2014-04-24 00:43:37 +0200

    Update comments
    Todo:
    - logic need to be fixed
    - ajax loading message
    - responsive design
    - icon, logo

--- WebContent/app.js
+++ WebContent/app.js
@@ -1,12 +1,13 @@
-// here's the content of the app
-// create a mobile App
-// it initializes the HTML page for mobile use and provides animated page handling
+// 	create a mobile App
+// 	it initializes the HTML page for mobile use and provides animated page handling
 var app = new sap.m.App("ShippingManager", {initialPage:"homepage"});
 
+//	shipment number and the path to the shipment detail
 var aShipment = "";
 var oPath = "";
 
-// create a list for displaying shipment details
+// 	create a list for displaying the detail of a shipment
+//	bind values to the data model
 var oList = new sap.m.List({
 	items: [
 	    new sap.m.DisplayListItem({
@@ -24,7 +25,7 @@ var oList = new sap.m.List({
 	]
 });
 
-// Button - Shipment start
+// 	create a button for updating the status of a shipment 
 var oButton = new sap.m.Button({
 	text: "Shipment start",
 	icon: "sap-icon://shipping-status",
@@ -32,42 +33,57 @@ var oButton = new sap.m.Button({
 	press: updateStatus
 });
 
-// create a Search Field
+// 	create a search field
 var oSearchField = new sap.m.SearchField({
 	placeholder: "shippment number",
 	width: "100%",
 	search: function(oEvent){
+		//	get the shipment number
 		aShipment = oEvent.getParameter("query");
+		
+		//	make sure that user has entered a shipment number
 		if(aShipment != "") {
-			
+			//	set path to this shipment
 			oPath = "/SHIPMENT('"+ aShipment +"')";
 	 		
-			// refresh OData model and read the data
+			// 	refresh OData model before reading it 
+			//	in case that service has been modified
 			oModel.refresh();
+			
+			//	read shipment detail
 			oModel.read(oPath, null, null, true, function(oData){
-				if(JSON.stringify(oData.Tknum) != '""' && oData.Tknum != undefined){
-					// Clear old list, rebind data, add new list
+				//	if success and shipment exist, Tknum returned (shipment number) should not be empty
+				if(JSON.stringify(oData.Tknum) != '""' && oData.Tknum != undefined) {
+					// clear old list, rebind data, add new list
+					/* not efficient, could be reviewed later */
 					homepage.removeContent(oList);
 					oList.bindElement(oPath);
     				homepage.addContent(oList);
     				
-    				if(JSON.stringify(oData.Sttrg) == '"5"')
+					//	if status is "5" (be careful, not 5, it's "5") 
+    				if(JSON.stringify(oData.Sttrg) == '"5"') {
     					oButton.setEnabled();
-    				else
+					} else {
     					oButton.setEnabled(false);
+					}
 				} else {
 					alert(aShipment + " doesn't exist");
 				}
 			},function(){
-				alert("Couldn't connect to the server");
+				//	read failed
+				alert("Couldn't find the service, please verify your network connection");
 			});
 		} else {
+			//	search field is empty
 			alert("Please enter a shipment number");
 		}
 	}
 });
 
-// create the first page of your application
+// 	create a page
+//	add oSearchField to sub header
+//	add oList to content
+//	add oButton to footer
 var homepage = new sap.m.Page("homepage", {
 	title: "Shipping Manager",
     showNavButton: false,
@@ -85,39 +101,52 @@ var homepage = new sap.m.Page("homepage", {
     })           
   });
 
-// add homepage to the App
+// 	add homepage to the App
 app.addPage(homepage); 
 
-// place the App into the HTML document
+// 	place the App into the HTML document
 app.placeAt("content"); 
 
-// update the status of a shipment
-function updateStatus(){
-	// disable the button after it is clicked already
+//	helper function
+// 	update the status of a shipment
+function updateStatus() {
+	//	disable the button after it has been clicked already
 	oButton.setEnabled(false);
-	// read update result
+	
+	// 	read update result
 	oModel2.read(oPath, null, null, true, function(oData){
-		if(JSON.stringify(oData.Status) == '"6"'){
-			// refresh OData model and read the data
+		//	if success, Status returned should be "6" 
+		if(JSON.stringify(oData.Status) == '"6"') {
+			// 	refresh OData model 
+			//	in case that service has been modified (someone has updated the status)
 			oModel.refresh();
+			
+			//	reload shipment detail
 			oModel.read(oPath, null, null, true, function(oData){
-				// Clear old list, rebind data, add new list
+				//	clear old list, rebind data, add new list
 				homepage.removeContent(oList);
 				oList.bindElement(oPath);
 				homepage.addContent(oList);
-				 
-				if(JSON.stringify(oData.Sttrg) == '"5"'){
-					oButton.setEnabled(true);
-				} else {
+				
+				//	if Sttrg (status) is still "5", means someone has updated the status (to 6) before you
+				//	your click just rolled back the status to 5
+				//	because the update request performs like clicking the button Shipment start in SAP system
+				if(JSON.stringify(oData.Sttrg) == '"5"') {
+					oButton.setEnabled();
+				} else if(JSON.stringify(oData.Sttrg) == '"6"') {
+					//	status is updated to 6
 					alert("Update successful");
+				} else {
+					//	someone has modified the status
+					return;
 				}
 			},function(){
 				return;
 			});
 		} else {
-			alert("Update failed, please it try again");
+			alert("Update failed, please refresh the shipment detail and try it again");
 		}
 	},function(){
-		alert("Connection failed");
+		alert("Couldn't find the service, please verify your network connection");
 	});
 }
